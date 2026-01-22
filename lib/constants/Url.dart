b6917757ff844cf35559987ead4a4a37cd212b7d
Aadarsh Mane <95363427+Aadarsh-Mane@users.noreply.github.com>
2026-01-22 10:04:19 +0530

    Update URLs to point to spandan.mattresswala.org

--- lib/constants/Url.dart
+++ lib/constants/Url.dart
@@ -1,4 +1,4 @@
-const KVM_URL = "https://testing.mattresswala.org";
-const VERCEL_URL = "https://testing.mattresswala.org";
+const KVM_URL = "https://spandan.mattresswala.org";
+const VERCEL_URL = "https://spandan.mattresswala.org";
 // const KVM_URL = "https://saideep.code2pdf.in";
-const BASE_URL = "https://testing.mattresswala.org";
+const BASE_URL = "https://spandan.mattresswala.org";
